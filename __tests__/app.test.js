1442c72960cd566b9274192f40305321383880a5
Dennis <dennis@Denniss-MacBook-Air.local>
2023-06-28 11:51:34 +0100

    resolved conflicts ticket 4

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,7 +1,7 @@
 const app = require("../app");
 const request = require("supertest"); // needed for integration testing to test for endpoints
 const db = require("../db/connection");
-const apiEndpoints = require('../endpoints.json')
+const apiEndpoints = require("../endpoints.json");
 const {
   articleData,
   commentData,
@@ -40,32 +40,34 @@ describe("GET /api/articles/:article_id", () => {
     return request(app)
       .get("/api/articles/1")
       .expect(200)
-      .then(({body}) => {
-        const {article}  = body;
-          expect(article.article_id).toEqual(1);
-          expect(article).toHaveProperty("title", expect.any(String));
-          expect(article).toHaveProperty("topic", expect.any(String));
-          expect(article).toHaveProperty("author", expect.any(String));
-          expect(article).toHaveProperty("body", expect.any(String));
-          expect(article).toHaveProperty("created_at", expect.any(String));
-          expect(article).toHaveProperty("votes", expect.any(Number));
-          expect(article).toHaveProperty("article_img_url", expect.any(String));
-  })
+      .then(({ body }) => {
+        const { article } = body;
+        expect(article.article_id).toEqual(1);
+        expect(article.title).toEqual("Living in the shadow of a great man");
+        expect(article.topic).toEqual("mitch");
+        expect(article.author).toEqual("butter_bridge");
+        expect(article.body).toEqual("I find this existence challenging");
+        expect(article.created_at).toEqual("2020-07-09T20:11:00.000Z");
+        expect(article.votes).toEqual(100);
+        expect(article.article_img_url).toEqual(
+          "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700"
+        );
+      });
   });
   test("400 reject an article_id with an invalid type of request", () => {
     return request(app)
       .get("/api/articles/badpath")
       .expect(400)
-      .then(({body}) => {
+      .then(({ body }) => {
         expect(body.message).toBe("Bad Request");
-  });
+      });
   });
   test("404 reject an article_id that is valid but not found", () => {
     return request(app)
       .get("/api/articles/100")
       .expect(404)
-      .then(({body}) => {
+      .then(({ body }) => {
         expect(body.message).toBe("Not Found");
-  });
+      });
   });
 });
